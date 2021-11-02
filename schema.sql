df626bf61f4e345550b62adc60fa2bda0e44f015
Manuel Andres Ruiz Faller <ruizfaller@hotmail,com>
2021-11-01 20:56:13 -0600

    Update

--- schema.sql
+++ schema.sql
@@ -36,4 +36,23 @@ CREATE TABLE salaries (
   PRIMARY KEY (emp_no)
 );
 
+CREATE TABLE Dept_Emp (
+	dept_no VARCHAR(4) NOT NULL,
+	emp_no INT NOT NULL,
+	from_date DATE NOT NULL,
+    to_date DATE NOT NULL,
+	FOREIGN KEY (emp_no) REFERENCES salaries (emp_no),
+	FOREIGN KEY (dept_no) REFERENCES departments (dept_no)
+	
+);
+
+CREATE TABLE Titles (
+	emp_no INT NOT NULL,
+	title VARCHAR(4) NOT NULL,
+	from_date DATE NOT NULL,
+    to_date DATE NOT NULL,
+	FOREIGN KEY (emp_no) REFERENCES employees (emp_no)
+	
+);
+
 SELECT * FROM departments;
